b4eb149b4cb70a114d9b6a43e47564226dc64dd0
Its-Masoom <2019ee43@student.uet.edu.pk>
2023-03-29 00:06:16 +0500

    lab001 all exercises solved

--- lab01/exercise1/ex1.c
+++ lab01/exercise1/ex1.c
@@ -6,14 +6,46 @@ There are two different ways to iterate through a string.
 1st way hint: strlen() may be useful
 2nd way hint: all strings end in a null terminator */
 int num_occurrences(char *str, char letter) {
-    /* TODO: implement num_occurances */
-    return 0;
+ int count = 0;
+    while (*str != '\0') {
+        if (*str == letter) {
+            count++;
+        }
+        str++;
+    }
+    return count;
 }
 
 /* Populates DNA_SEQ with the number of times each nucleotide appears.
 Each sequence will end with a NULL terminator and will have up to 20 nucleotides.
 All letters will be upper case. */
 void compute_nucleotide_occurrences(DNA_sequence *dna_seq) {
-    /* TODO: implement compute_nucleotide_occurances */
+    int i = 0;
+    int count_A = 0, count_C = 0, count_G = 0, count_T = 0;
+    
+    while(dna_seq->sequence[i] != '\0') {
+        switch(dna_seq->sequence[i]) {
+            case 'A':
+                count_A++;
+                break;
+            case 'C':
+                count_C++;
+                break;
+            case 'G':
+                count_G++;
+                break;
+            case 'T':
+                count_T++;
+                break;
+            default:
+                break;
+        }
+        i++;
+    }
+    
+    dna_seq->A_count = count_A;
+    dna_seq->C_count = count_C;
+    dna_seq->G_count = count_G;
+    dna_seq->T_count = count_T;   
     return;
 }
--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -3,4 +3,24 @@
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+    // Initialize the fast and slow pointers
+    node *fast_ptr = head;
+    node *slow_ptr = head;
+
+    // Traverse the list with fast and slow pointers
+    while (fast_ptr && fast_ptr->next) {
+        // Advance the fast pointer by two nodes
+        fast_ptr = fast_ptr->next->next;
+
+        // Advance the slow pointer by one node
+        slow_ptr = slow_ptr->next;
+
+        // If the fast and slow pointers ever point to the same node, the list is cyclic
+        if (fast_ptr == slow_ptr) {
+            return 1;
+        }
+    }
+
+    // If we reach the end of the list, it is acyclic
+    return 0;
 }
